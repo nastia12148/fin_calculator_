a346f930f4946a73d4873ddecff4f2dd8a123034
nastia12148 <nastia12149@gmail.com>
2022-10-11 17:01:26 +0300

    added title with personal information, added black&flake8, fixed operations with big numbers

--- main.py
+++ main.py
@@ -1,31 +1,43 @@
+from unicodedata import decimal
+
 import PySimpleGUI as sg
 import re
-import hashlib
+from decimal import *
 
 layout = [
-    [sg.InputText(), sg.InputOptionMenu(['+', '-']), sg.InputText(), sg.Button('='), sg.Output(key='-OUT-', size=(80,20))
-     ],
-    [sg.Submit(), sg.Cancel()]
-]
-window = sg.Window('File Compare', layout).finalize()
+    [sg.Text("Marynichava Anastasiya Egorovna, 4th course, 4th group, 2022")],
+    [
+        sg.InputText(),
+        sg.InputOptionMenu(["+", "-"]),
+        sg.InputText(),
+        sg.Button("="),
+        sg.Output(key="-OUT-"),
+    ],
+    ]
+window = sg.Window("File Compare", layout).finalize()
 while True:  # The Event Loop
     event, values = window.read(timeout=0)
 
-    # print(event, values) #debug
-    if event in (None, 'Exit', 'Cancel'):
+    if event in (None, "Exit"):
         break
-    if event == '=':
+    if event == "=":
         if not values[0] or not values[2]:
-            window.FindElement('-OUT-').Update('You need to input values')
-        elif not re.match(r'([+-]?[0-9]+[.,]?[0-9]*$)', values[0]) or not re.match(r'([+-]?[0-9]+[.,]?[0-9]*$)', values[2]):
-            window.FindElement('-OUT-').Update('Wrong input values')
-        elif abs(float(values[0])) > 1000000000 or abs(float(values[2])) > 1000000000:
-            window.FindElement('-OUT-').Update('Input value is very big!')
-        else:
-            if values[1] == '+':
-                window.FindElement('-OUT-').Update(float(values[0]) + float(values[2]))
-            elif values[1] == '-':
-                window.FindElement('-OUT-').Update(float(values[0]) - float(values[2]))
-            else:
-                window.FindElement('-OUT-').Update('You need to choose the operation')
+            window.FindElement("-OUT-").Update("You need to input values")
 
+        elif not re.match(r"([+-]?[0-9]+[.,]?[0-9]*$)", values[0]) or not re.match(
+            r"([+-]?[0-9]+[.,]?[0-9]*$)", values[2]
+        ):
+            window.FindElement("-OUT-").Update("Wrong input values")
+
+        elif abs(Decimal(values[0])) > 1000000000000 or abs(Decimal(values[2])) > 1000000000000:
+            window.FindElement("-OUT-").Update("Input value is very big!")
+
+        else:
+            if values[1] == "+":
+                window.FindElement("-OUT-").Update(Decimal(Decimal(values[0]) + Decimal(values[2])))
+
+            elif values[1] == "-":
+                window.FindElement("-OUT-").Update(Decimal(Decimal(values[0]) - Decimal(values[2])))
+
+            else:
+                window.FindElement("-OUT-").Update("You need to choose the operation")
