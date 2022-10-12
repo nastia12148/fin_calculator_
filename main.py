567b2f900bbcf37b26e1b26e453c266713af3833
nastia12148 <nastia12149@gmail.com>
2022-10-12 15:36:44 +0300

    fixed input and result exceptions

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from unicodedata import decimal
-
 import PySimpleGUI as sg
 import re
 from decimal import *
@@ -11,11 +10,12 @@ layout = [
         sg.InputOptionMenu(["+", "-"]),
         sg.InputText(),
         sg.Button("="),
-        sg.Output(key="-OUT-"),
+        sg.Output(key="-OUT-", size=(80, 5)),
     ],
     ]
-window = sg.Window("File Compare", layout).finalize()
-while True:  # The Event Loop
+window = sg.Window("Financial calculator", layout).finalize()
+
+while True:
     event, values = window.read(timeout=0)
 
     if event in (None, "Exit"):
@@ -31,14 +31,22 @@ while True:  # The Event Loop
             window.FindElement("-OUT-").Update("Wrong input values")
 
         elif abs(Decimal(values[0])) > 1000000000000 or abs(Decimal(values[2])) > 1000000000000:
-            window.FindElement("-OUT-").Update("Input value is very big!")
+            window.FindElement("-OUT-").Update("The modulus of the input value is very big!")
 
+        elif abs(Decimal(values[0])) < 0.000000000001 or abs(Decimal(values[2])) < 0.000000000001:
+            window.FindElement("-OUT-").Update("The modulus of the input value is very small!")
         else:
             if values[1] == "+":
-                window.FindElement("-OUT-").Update(Decimal(Decimal(values[0]) + Decimal(values[2])))
+                if abs(Decimal(values[0]) + Decimal(values[2])) <= 1000000000000:
+                    window.FindElement("-OUT-").Update(Decimal(Decimal(values[0]) + Decimal(values[2])))
+                else:
+                    window.FindElement("-OUT-").Update("The modulus of the sum more than 1 000 000 000 000!")
 
             elif values[1] == "-":
-                window.FindElement("-OUT-").Update(Decimal(Decimal(values[0]) - Decimal(values[2])))
+                if abs(Decimal(values[0]) - Decimal(values[2])) <= 1000000000000:
+                    window.FindElement("-OUT-").Update(Decimal(Decimal(values[0]) - Decimal(values[2])))
+                else:
+                    window.FindElement("-OUT-").Update("The modulus of the difference more than 1 000 000 000 000!")
 
             else:
                 window.FindElement("-OUT-").Update("You need to choose the operation")
