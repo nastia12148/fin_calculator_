00ba2a0cf438e89c15ca8a4ac492920e69ca736b
nastia12148 <nastia12149@gmail.com>
2022-10-19 17:51:44 +0300

    step 2 completed

--- main.py
+++ main.py
@@ -25,32 +25,37 @@ while True:
         if not values[0] or not values[2]:
             window.FindElement("-OUT-").Update("You need to input values")
 
-        elif not re.match(r"([+-]?[0-9]+[.,]?[0-9]*$)", values[0]) or not re.match(
-            r"([+-]?[0-9]+[.,]?[0-9]*$)", values[2]
-        ):
-            window.FindElement("-OUT-").Update("Wrong input values")
+        elif not re.match(r"\d",values[0]) or not re.match(r"\d", values[2]):
+            window.FindElement("-OUT-").Update("Your input must contain only numbers and only 1 '.' or ',' symbol")
 
-        elif abs(Decimal(values[0])) > 1000000000000 or abs(Decimal(values[2])) > 1000000000000:
+        elif not re.match(r"([+-]?[0-9]+[.,]?[0-9]*$)|([+-]?(([1-9])|([1-9][0-9])|([1-9][0-9][0-9]))([ ][0-9][0-9][0-9])*[.,]?[0-9]*$)", values[0]) or not re.match(
+            r"([+-]?[0-9]+[.,]?[0-9]*$)|([+-]?(([1-9])|([1-9][0-9])|([1-9][0-9][0-9]))([ ][0-9][0-9][0-9])*[.,]?[0-9]*$)", values[2]
+        ):
+            window.FindElement("-OUT-").Update("Incorrect amount or placement of spaces")
+
+        elif abs(Decimal(values[0].replace(" ", ""))) > 1000000000000 or abs(Decimal(values[2].replace(" ", ""))) > 1000000000000:
             window.FindElement("-OUT-").Update("The modulus of the input value is very big!")
 
-        elif abs(Decimal(values[0])) < 0.000001 or abs(Decimal(values[2])) < 0.000001:
-            window.FindElement("-OUT-").Update("The modulus of the input value is very small!")
+        elif (abs(Decimal(values[0].replace(" ", "")))*10000000 % 10 != 0) or (abs(Decimal(values[2].replace(" ", "")))*10000000 % 10) != 0:
+            window.FindElement("-OUT-").Update("Fractional part of modulus of the input value contain more than 6 numbers!")
         else:
+            values[0] = values[0].replace(" ", "")
+            values[2] = values[2].replace(" ", "")
             if values[1] == "+":
                 if abs(Decimal(values[0]) + Decimal(values[2])) <= 1000000000000:
-                    window.FindElement("-OUT-").Update(format(Decimal(Decimal(values[0]) + Decimal(values[2])).normalize(), "1.6f"))
+                    window.FindElement("-OUT-").Update('{0:,}'.format(Decimal(Decimal(values[0]) + Decimal(values[2])).normalize(), "1.6f").replace(",", " "))
                 else:
                     window.FindElement("-OUT-").Update("The modulus of the sum more than 1 000 000 000 000!")
 
             elif values[1] == "-":
                 if abs(Decimal(values[0]) - Decimal(values[2])) <= 1000000000000:
-                    window.FindElement("-OUT-").Update(format(Decimal(Decimal(values[0]) - Decimal(values[2])).normalize(), "1.6f"))
+                    window.FindElement("-OUT-").Update('{0:,}'.format(Decimal(Decimal(values[0]) - Decimal(values[2])).normalize(), "1.6f").replace(",", " "))
                 else:
                     window.FindElement("-OUT-").Update("The modulus of the difference more than 1 000 000 000 000!")
 
             elif values[1] == "*":
                 if abs(Decimal(values[0]) * Decimal(values[2])) <= 1000000000000:
-                    window.FindElement("-OUT-").Update(Decimal(Decimal(values[0]) * Decimal(values[2])).normalize())
+                    window.FindElement("-OUT-").Update('{0:,}'.format(Decimal(Decimal(values[0]) * Decimal(values[2])).normalize(), "1.6f").replace(",", " "))
                 else:
                     window.FindElement("-OUT-").Update("The modulus of the multiplication more than 1 000 000 000 000!")
 
@@ -59,7 +64,7 @@ while True:
                     window.FindElement("-OUT-").Update("Division by zero error!")
                 if abs(Decimal(values[0]) * Decimal(values[2])) <= 1000000000000:
                     window.FindElement("-OUT-").\
-                        Update(Decimal(Decimal(values[0]) / Decimal(values[2])).quantize(Decimal("0.000001")).normalize())
+                        Update('{0:,}'.format(Decimal(Decimal(values[0]) / Decimal(values[2])).quantize(Decimal("0.000001")).normalize(), "1.6f").replace(",", " "))
                 else:
                     window.FindElement("-OUT-").Update("The modulus of the division more than 1 000 000 000 000!")
 
